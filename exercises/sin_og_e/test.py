b47d5e40c4c0b1ea9f02f702777eceafe4628550
Jørund Jenssen Berg <jjberg@gmail.com>
2023-08-25 09:20:24 +0200

    Fant tross alt ut en del, men langtfra ferdig

---
+++ exercises/sin_og_e/test.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+# sinus = np.sin(0)
+# print(sinus)
+# sinus = np.sin(np.pi/2.)
+# print(sinus)
+# sinus = np.sin(1*np.pi)
+# print(sinus)
+# sinus = np.sin(-(np.pi/2.))
+# print(sinus)
+# print(np.pi)
+
+piTing = np.array((0., 30., 45., 60., 90.)) * np.pi / 180.
+print(piTing) # kommer ut i grader
+# formel konversjon
+# rad/pi = grader/180
