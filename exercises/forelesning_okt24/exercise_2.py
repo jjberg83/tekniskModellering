00017dfeed94bac3364d0b81d6535214bef02020
Jørund Jenssen Berg <jjberg@gmail.com>
2023-10-26 11:51:05 +0200

    Bare endringer relatert til at jeg repeterte materialet

--- exercises/forelesning_okt24/exercise_2.py
+++ exercises/forelesning_okt24/exercise_2.py
@@ -11,25 +11,29 @@ df = pd.read_excel('DrillingData.xlsx')
 # %%
 # Get info about the data
 # Vi ser at vi har int (vil ha floats) og objects 
-#df.info()                   
+df.info()                   
 # %%
-#df.tail(10)
+df.head(10)
+df.tail(10)
 # %%
 # Får bare x2 nå, fordi den er den eneste som bare har numeric
-#df.describe()
+df.describe()
 # %%
 # Object and int should be converted to float
 df['x1'] = pd.to_numeric(df['x1'], errors='coerce') #coerce gjør at iinvalid entries blir NaN
 df['x3'] = pd.to_numeric(df['x3'], errors='coerce') #coerce gjør at iinvalid entries blir NaN
 df['x4'] = pd.to_numeric(df['x4'], errors='coerce') #coerce gjør at iinvalid entries blir NaN
 df['y'] = pd.to_numeric(df['y'], errors='coerce') #coerce gjør at iinvalid entries blir NaN
+df.head(10)
+df.tail(10)
+# df.describe() # alle kolonner vises nå, siden alle kolonner er Numeric
 # %%
 # To convert int to float
 df['y'] = df['y'].astype(float)
 df['x2'] = df['x2'].astype(float)
 
 # Alternativt
-df['y'] = pd.to_numeric(df['y'], downcast='float')
+# df['y'] = pd.to_numeric(df['y'], downcast='float')
 # %%
 # sjekker data på ny for å se etter nye feil
 # ser non-null og int 64
@@ -45,6 +49,7 @@ df.dropna(inplace = True)
 # %%
 # Vi ser at vi har 21505 entries, og Non_Null kolonnen har bare 21505
 df.info()
+df.tail(10)
 # %%
 # Remove duplicated
 dups = df.duplicated()
